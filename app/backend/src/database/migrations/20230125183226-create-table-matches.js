8ce5de06d00323fbf5bf5ce14778db52ec34cca7
Victor Matias <fvictor1705@gmail.com>
2023-01-25 15:42:19 -0300

    feat: create migration 'create-table-matches'

---
+++ app/backend/src/database/migrations/20230125183226-create-table-matches.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      home_team_Id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+      },
+      home_team_goals: {
+        type: Sequelize.INTEGER,
+      },
+      away_team_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+      },
+      away_team_goals: {
+        type: Sequelize.INTEGER,
+      },
+      in_progress: {
+        type: Sequelize.BOOLEAN,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('matches');
+  }
+};
