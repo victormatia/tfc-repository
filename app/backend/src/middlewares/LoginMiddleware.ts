c0c20938d9ae6b67c12faa32a13dcd209e528b3c
Victor Matias <fvictor1705@gmail.com>
2023-01-26 18:36:59 -0300

    feat: create validateToken static method

--- app/backend/src/middlewares/LoginMiddleware.ts
+++ app/backend/src/middlewares/LoginMiddleware.ts
@@ -1,15 +1,27 @@
-import { Request, Response, NextFunction, RequestHandler } from 'express';
+import { RequestHandler } from 'express';
+import JWT from '../auth/JWT';
 
 export default class LoginMiddleware {
-  public static validateFields: RequestHandler = async (
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ) => {
+  public static validateFields: RequestHandler = async (req, res, next) => {
     const { email, password } = req.body;
 
     if (!email || !password) return res.status(400).json({ message: 'All fields must be filled' });
 
     next();
+  };
+
+  public static validateToken: RequestHandler = async (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if (authorization) {
+      const jwt = new JWT();
+      const { userInfos } = jwt.verifyToken(authorization);
+
+      req.body.user = userInfos;
+
+      return next();
+    }
+
+    res.status(400).json({ message: 'authorization field not found' });
   };
 }
