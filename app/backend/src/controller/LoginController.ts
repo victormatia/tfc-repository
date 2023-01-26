fa572a441fbd5eb4faac6ba8676e23cea3d6fa18
Victor Matias <fvictor1705@gmail.com>
2023-01-26 18:38:52 -0300

    feat: create get static method

--- app/backend/src/controller/LoginController.ts
+++ app/backend/src/controller/LoginController.ts
@@ -1,8 +1,8 @@
-import { Request, RequestHandler, Response } from 'express';
+import { RequestHandler } from 'express';
 import LoginService from '../service/LoginService';
 
 export default class LoginController {
-  public static post: RequestHandler = async (req: Request, res: Response) => {
+  public static post: RequestHandler = async (req, res) => {
     const { email, password } = req.body;
 
     const login = new LoginService();
@@ -11,5 +11,11 @@ export default class LoginController {
     if (error) return res.status(401).json({ message: error.message });
 
     res.status(200).json({ token });
+  };
+
+  public static get:RequestHandler = async (req, res) => {
+    const { user: { role } } = req.body;
+
+    res.status(200).json({ role });
   };
 }
