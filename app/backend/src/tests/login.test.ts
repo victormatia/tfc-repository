020a43a0a3634938ac092d94671f91ac0c7692df
Victor Matias <fvictor1705@gmail.com>
2023-01-27 17:31:34 -0300

    test: add test case to /login/validate endpoint

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -30,4 +30,20 @@ describe('Testes de integração para o endpoit /login', () => {
     expect(chaiHttpResponse.body).to.have.property('token');
     expect(chaiHttpResponse.status).to.be.equal(200);
   });
+
+  it('Verifica se a role do usuário logado é retornada corretamente', async () => {
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3MiOnsiaWQiOjEsInVzZXJuYW1lIjoiQWRtaW4iLCJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInBhc3N3b3JkIjoiJDJhJDA4JHhpLkh4azFjekFPMG5aUi4uQjM5M3UxMGFFRDBSUTFOM1BBRVhRN0h4dExqS1BFWkJ1LlBXIiwicm9sZSI6ImFkbWluIn0sImlhdCI6MTY3NDg1MDU5NCwiZXhwIjoxNjc0OTM2OTk0fQ.vU4L29SVQ277Syh5p3VbPrEsUtEOsVoz826qaEV-JL8';
+
+    chaiHttpResponse = await chai.request(app).get('/login/validate').set('authorization', token);
+
+    expect(chaiHttpResponse.body).to.be.deep.equal({ role: 'admin' });
+    expect(chaiHttpResponse.status).to.be.equal(200);
+  });
+
+  it('Verifica se a requisição retorna uma messagem de erro quando não possui a chave authorization', async () => {
+    chaiHttpResponse = await chai.request(app).get('/login/validate');
+
+    expect(chaiHttpResponse.body).to.be.deep.equal({ message: 'authorization field not found' });
+    expect(chaiHttpResponse.status).to.be.equal(400);
+  });
 })
