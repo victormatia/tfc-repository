1661b9a4b1ff768c292e8cdacf173b7fdf44bbd6
Victor Matias <fvictor1705@gmail.com>
2023-01-27 18:05:46 -0300

    feat: add route for /teams

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
-import LoginRoute from './routes/LoginRoute';
+import TeamRoute from './routes/Team.routes';
+import LoginRoute from './routes/Login.routes';
 
 class App {
   public app: express.Express;
@@ -12,6 +13,7 @@ class App {
     // Não remover essa rota
     this.app.get('/', (req, res) => res.json({ ok: true }));
     this.app.use('/login', LoginRoute);
+    this.app.use('/teams', TeamRoute);
   }
 
   private config():void {
