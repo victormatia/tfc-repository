99d3f435f8b776dc2875ce5385be2ab8cf8351a5
Victor Matias <fvictor1705@gmail.com>
2023-01-27 18:32:05 -0300

    feat: add route for /teams/:id

--- app/backend/src/routes/Team.routes.ts
+++ app/backend/src/routes/Team.routes.ts
@@ -4,5 +4,6 @@ import TeamController from '../controller/TeamController';
 const route = Router();
 
 route.get('/', TeamController.getAll);
+route.get('/:id', TeamController.getById);
 
 export default route;
