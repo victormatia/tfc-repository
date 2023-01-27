407a2e427b6c86f553a0273d398f2cd90a147ef9
Victor Matias <fvictor1705@gmail.com>
2023-01-27 16:14:52 -0300

    test: create mock for model User

---
+++ app/backend/src/tests/mocks/mockModels.ts
@@ -0,0 +1,9 @@
+const userModelFindOne = {
+  id: 1,
+  username: 'Admin',
+  email: 'admin@admin.com',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+  role: 'admin'
+}
+
+export default userModelFindOne;
