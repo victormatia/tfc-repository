50e6658337ca2851f6513aece9fa916d5c67903d
Victor Matias <fvictor1705@gmail.com>
2023-01-28 17:19:09 -0300

    feat: create routes file for /matches

---
+++ app/backend/src/routes/Match.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import MatchController from '../controller/MatchController';
+
+const route = Router();
+
+route.get('/', new MatchController().getAll);
+
+export default route;
