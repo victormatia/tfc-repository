a6c4ef5b837a88c44fb65e2ea32b75a10708e4bd
Victor Matias <fvictor1705@gmail.com>
2023-01-29 17:49:46 -0300

    feat: add post route for /matches and patch route for /matches/:id/finish

--- app/backend/src/routes/Match.routes.ts
+++ app/backend/src/routes/Match.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import LoginMiddleware from '../middlewares/LoginMiddleware';
 import MatchController from '../controller/MatchController';
 
 const route = Router();
 
 route.get('/', new MatchController().getAll);
 route.get('/?inProgress', new MatchController().getAll);
+route.post('/', LoginMiddleware.validateToken, new MatchController().postMatch);
+route.patch('/:id/finish', LoginMiddleware.validateToken, new MatchController().finishMatch);
 
 export default route;
