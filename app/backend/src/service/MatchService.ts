1be9cca82edc97697f4fa760582342d61b2f1bfc
Victor Matias <fvictor1705@gmail.com>
2023-01-29 17:36:21 -0300

    refactor: apply typing to getAllMatches, getAllMatchesByProgress methods; feat: creates postMatch, finishMatch and validateTeams methods.

--- app/backend/src/service/MatchService.ts
+++ app/backend/src/service/MatchService.ts
@@ -1,30 +1,79 @@
 import Team from '../database/models/Team';
 import Match from '../database/models/Match';
+import { IError, IMatch } from '../interfaces';
 
 export default class MatchService {
-  constructor(private _match = Match) {
+  constructor(
+    private _match = Match,
+    private _team = Team,
+  ) {
     this._match = _match;
+    this._team = _team;
   }
 
-  public async getAllMatches() {
+  public async getAllMatches(): Promise<IMatch[]> {
     const matches = await this._match.findAll({
       include: [
         { model: Team, association: 'homeTeam', attributes: ['teamName'] },
         { model: Team, association: 'awayTeam', attributes: ['teamName'] },
       ],
-    });
+    }) as unknown;
 
-    return matches;
+    return matches as IMatch[];
   }
 
-  public async getAllMatchesByProgress(progress: boolean) {
+  public async getAllMatchesByProgress(progress: boolean): Promise<IMatch[]> {
     const matches = await this._match.findAll({ where: { inProgress: progress },
       include: [
         { model: Team, association: 'homeTeam', attributes: ['teamName'] },
         { model: Team, association: 'awayTeam', attributes: ['teamName'] },
       ],
-    });
+    }) as unknown;
 
-    return matches;
+    return matches as IMatch[];
+  }
+
+  public async postMatch(match: IMatch): Promise<{ result?: IMatch, error?: IError }> {
+    const { homeTeamId, awayTeamId } = match;
+    const areValidTeams = await this.validateTeams([homeTeamId, awayTeamId]);
+
+    if (!areValidTeams) {
+      return { error: {
+        code: 404,
+        message: 'There is no team with such id!',
+      } };
+    }
+
+    if (homeTeamId === awayTeamId) {
+      return { error: {
+        code: 422,
+        message: 'It is not possible to create a match with two equal teams',
+      } };
+    }
+
+    const addedMatch = await this._match.create({ ...match, inProgress: true }) as unknown;
+
+    return { result: addedMatch as IMatch };
+  }
+
+  public async finishMatch(id: number) {
+    const updateProgress = await this._match.update({ inProgress: false }, { where: { id } });
+
+    if (updateProgress[0]) {
+      return { message: 'Finished' };
+    }
+
+    return { message: 'the match already was finished' };
+  }
+
+  private async validateTeams(teamsId: number[]) {
+    const teams = await Promise.all(teamsId.map(async (id) => {
+      const team = await this._team.findByPk(id);
+      return team;
+    }));
+
+    const areValidTeams = teams.every((team) => team);
+
+    return areValidTeams;
   }
 }
