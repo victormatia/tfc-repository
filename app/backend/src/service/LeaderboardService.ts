3db2ac8e516a076dbdc5111b6e002ac1dccfeaf8
Victor Matias <fvictor1705@gmail.com>
2023-02-04 20:11:48 -0300

    feat: create hankTeams method

--- app/backend/src/service/LeaderboardService.ts
+++ app/backend/src/service/LeaderboardService.ts
@@ -3,6 +3,7 @@ import Match from '../database/models/Match';
 import { IHanked, IMatch, ITeam } from '../interfaces';
 import createHankedHomeTeamsArr from './utils/LeaderboardHome';
 import createHankedAwayTeamsArr from './utils/LeaderboardAway';
+import createHankedTeamsArr from './utils/LeaderrboardGeneral';
 
 const sortArr = (a: IHanked, b: IHanked) => b.totalPoints - a.totalPoints
 || b.totalVictories - a.totalVictories
@@ -42,6 +43,22 @@ export default class LeaderBoardService {
     const teams = await this._team.findAll() as unknown;
 
     const arrHankedTeams = createHankedAwayTeamsArr(teams as ITeam[], finishedMtaches as IMatch[]);
+
+    return arrHankedTeams
+      .sort(sortArr);
+  }
+
+  public async hankTeams(): Promise<IHanked[]> {
+    const finishedMtaches = await this._match.findAll({ where: { inProgress: false },
+      include: [
+        { model: Team, association: 'homeTeam', attributes: ['teamName'] },
+        { model: Team, association: 'awayTeam', attributes: ['teamName'] },
+      ],
+    }) as unknown;
+
+    const teams = await this._team.findAll() as unknown;
+
+    const arrHankedTeams = createHankedTeamsArr(teams as ITeam[], finishedMtaches as IMatch[]);
 
     return arrHankedTeams
       .sort(sortArr);
