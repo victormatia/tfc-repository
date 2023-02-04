7140359c91de2c482932f728cfb26ebbef91dc88
Victor Matias <fvictor1705@gmail.com>
2023-02-04 15:00:53 -0300

    feat: create getAway method

--- app/backend/src/controller/LeaderboardController.ts
+++ app/backend/src/controller/LeaderboardController.ts
@@ -6,8 +6,14 @@ export default class LeaderBoardController {
     private _service = new LeaderBoardService(),
   ) { this._service = _service; }
 
-  public get: RequestHandler = async (_req, res) => {
-    const hankedTeams = await this._service.hankTeams();
+  public getHome: RequestHandler = async (_req, res) => {
+    const hankedTeams = await this._service.hankHomeTeams();
+
+    res.status(200).json(hankedTeams);
+  };
+
+  public getAway: RequestHandler = async (_req, res) => {
+    const hankedTeams = await this._service.hankAwayTeams();
 
     res.status(200).json(hankedTeams);
   };
