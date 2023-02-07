531854742c7b3482f67ad6cc78375d0a5799dab5
Victor Matias <fvictor1705@gmail.com>
2023-02-07 13:42:37 -0300

    test: add new test cases for route /matches

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -6,6 +6,7 @@ import { Response } from 'superagent';
 import { app } from '../app';
 import Match from '../database/models/Match';
 import { matcheModelFindAll } from './mocks/mockModels';
+import Team from '../database/models/Team';
 
 chai.use(chaiHttp);
 
@@ -13,6 +14,7 @@ const { expect } = chai;
 
 describe('Testes de integração para o endpoint /match', () => {
   let response: Response;
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3MiOnsiaWQiOjEsInVzZXJuYW1lIjoiQWRtaW4iLCJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInBhc3N3b3JkIjoiJDJhJDA4JHhpLkh4azFjekFPMG5aUi4uQjM5M3UxMGFFRDBSUTFOM1BBRVhRN0h4dExqS1BFWkJ1LlBXIiwicm9sZSI6ImFkbWluIn0sImlhdCI6MTY3NTc4NjQ2MCwiZXhwIjoxNjc1ODcyODYwfQ._IW8eYfp-4dINQnOZJANdJdUltTXrLaZUI6_U4ssE7w'
 
   afterEach(restore);
 
@@ -22,6 +24,57 @@ describe('Testes de integração para o endpoint /match', () => {
     response = await chai.request(app).get('/matches');
 
     expect(response.body).to.be.deep.equal(matcheModelFindAll);
+    expect(response.status).to.be.equal(200);
+  });
+
+  it('Verifica se é possível adicionar uma nova partida', async () => {
+    stub(Match, 'create').resolves({
+      id: 49,
+      homeTeamId: 16,
+      awayTeamId: 8,
+      homeTeamGoals: 2,
+      awayTeamGoals: 2,
+      inProgress: true
+    } as any);
+
+    response = await chai
+      .request(app)
+      .post('/matches')
+      .set('authorization', token)
+      .send({
+        "homeTeamId": 16,
+        "awayTeamId": 8,
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 2
+      });
+
+    expect(response.body).to.be.deep.equal({
+      id: 49,
+      homeTeamId: 16,
+      awayTeamId: 8,
+      homeTeamGoals: 2,
+      awayTeamGoals: 2,
+      inProgress: true
+    });
+
+    expect(response.status).to.be.equal(201);
+  });
+
+  it('Verifica se é possível finalizar uma partida em andamento', async () => {
+    stub(Match, 'update').resolves([1]);
+
+    response = await chai.request(app).patch('/matches/49/finish').set('authorization', token);
+
+    expect(response.body).to.be.deep.equal({ message: 'Finished' });
+    expect(response.status).to.be.equal(200);
+  });
+
+  it('Verifica se não é possível finalizar uma partida finalizada', async () => {
+    stub(Match, 'update').resolves([0]);
+
+    response = await chai.request(app).patch('/matches/49/finish').set('authorization', token);
+
+    expect(response.body).to.be.deep.equal({ message: 'the match already was finished' });
     expect(response.status).to.be.equal(200);
   });
     
